4c2ec5103eb372f0ae5ba9040ee5322516d0adc7
zo958723 <zo958723@ohio.edu>
2026-02-02 15:09:04 -0500

    putting file changes onto github

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Zak Ottersbach
+ *        Date: 02/02/2026
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
